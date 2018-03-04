6ebcebac802520f36706368d649f9d7036e109e4
vunhattruong <vunhtruong@gmail.com>
2018-03-04 20:16:28 +0700

    Add files via upload

---
+++ input.js
@@ -0,0 +1,58 @@
+'use strict';
+
+var fs = require('fs');
+var when = require('when');
+
+/**
+ * Create a input method for reading HTML file
+ * @constructor
+ */
+function Input() {
+}
+
+/**
+ * Open a file from file path
+ * @param {string} filePath - the location of the HTML file
+ */
+Input.prototype.createInputFile = function(filePath) {
+
+    // read function
+    return function(){
+        var deferred = when.defer();
+        fs.readFile(filePath, (err, data) => {
+            if (err) return deferred.reject(err);
+            var rawHtml = data.toString().replace((/  |\r\n|\n|\r/gm), "");
+            deferred.resolve(rawHtml);
+        });
+        return deferred.promise;
+    };
+}
+
+/**
+ * Open a node readable stream
+ * @param {object} stream - A node readable stream
+ */
+Input.prototype.createInputStream = function(stream) {
+
+    // read function
+    return function(){
+        var deferred = when.defer();
+        var rawHtml = "";
+        if (typeof stream !== "object" && typeof stream.on !== "function") {
+            deferred.reject("Invalid stream object");
+        } else {
+            stream.on('data', (buf) => {
+                rawHtml = buf.toString().replace((/  |\r\n|\n|\r/gm),"");
+            });
+            stream.on('error', (err) => {
+                deferred.reject(err);
+            });
+            stream.on('end', () => {
+                deferred.resolve(rawHtml);
+            });
+        }
+        return deferred.promise;
+    };
+}
+
+module.exports = Input;
---
+++ output.js
@@ -0,0 +1,56 @@
+'use strict';
+
+var fs = require('fs');
+
+/**
+ * Create a output method for showing defect result
+ * @constructor
+ */
+function Output() {
+}
+
+/**
+ * Create a console
+ */
+Output.prototype.createOutputConsole = function() {
+
+    // write function
+    return function(msg){
+        console.log(msg);
+    }
+}
+
+/**
+ * Open a file from file path
+ * @param {string} filePath - the location of the HTML file
+ */
+Output.prototype.createOutputFile = function(filePath) {
+
+    // write function
+    return function(msg){
+        fs.writeFile(filePath, msg, function (err){
+            if(err){
+                throw err;
+            }
+        });
+    }
+}
+
+/**
+ * Open a node writeable stream
+ * @param {object} stream - A node writeable stream
+ */
+Output.prototype.createOutputStream = function(stream) {
+
+    // write function
+    return function(msg){
+        stream.write(msg, function (err){
+            if(err){
+                throw err;
+            }
+        });
+        stream.end();
+    }
+}
+
+module.exports = Output;
